6b35a82795e081fd17f78803b209d8ec1206b27f
Joenell Canlas Alonzo <medusazxc123@gmail.com>
2023-12-06 22:34:57 +0800

    Create UI for Vital Signs, Past History and Social History in Patient Chart

--- src/pages/Patient/PatientChart.vue
+++ src/pages/Patient/PatientChart.vue
@@ -100,16 +100,189 @@
                 <div class="row mt-4">
                     <div class="col-sm-12 col-md-6 col-lg-3">
                         <div>
-                            <label class="form-label fs-6 mb-2 fw-semibold">Height (feet / inches)</label>
+                            <label class="form-label fs-6 mb-2 fw-semibold">Height (cm)</label>
+                            <input type="text" class="form-control form-control-sm">
+                        </div>
+                    </div>
+                    <div class="col-sm-12 col-md-6 col-lg-3">
+                        <div>
+                            <label class="form-label fs-6 mb-2 fw-semibold">Weight (kg)</label>
+                            <input type="text" class="form-control form-control-sm">
+                        </div>
+                    </div>
+                    <div class="col-sm-12 col-md-6 col-lg-3">
+                        <div>
+                            <label class="form-label fs-6 mb-2 fw-semibold">BMI (Body Mass Index)</label>
+                            <input type="text" class="form-control form-control-sm">
+                        </div>
+                    </div>
+                    <div class="col-sm-12 col-md-6 col-lg-3">
+                        <div>
+                            <label class="form-label fs-6 mb-2 fw-semibold">Blood Pressure</label>
                             <div class="d-flex gap-1">
                                 <input type="text" class="form-control form-control-sm">
+                                <span>/</span>
                                 <input type="text" class="form-control form-control-sm">
                             </div>
                         </div>
                     </div>
+
+                </div>
+                <div class="row mt-3">
+                    <div class="col-sm-12 col-md-6 col-lg-3">
+                        <div>
+                            <label class="form-label fs-6 mb-2 fw-semibold">Oxygen Saturation</label>
+                            <input type="text" class="form-control form-control-sm">
+                        </div>
+                    </div>
+                    <div class="col-sm-12 col-md-6 col-lg-3">
+                        <div>
+                            <label class="form-label fs-6 mb-2 fw-semibold">Temperature (°C)</label>
+                            <input type="text" class="form-control form-control-sm">
+                        </div>
+                    </div>
+                    <div class="col-sm-12 col-md-6 col-lg-3">
+                        <div>
+                            <label class="form-label fs-6 mb-2 fw-semibold">Respiratory Rate (RR)</label>
+                            <input type="text" class="form-control form-control-sm">
+                        </div>
+                    </div>
+                    <div class="col-sm-12 col-md-6 col-lg-3">
+                        <div>
+                            <label class="form-label fs-6 mb-2 fw-semibold">Pulse Rate (PR)</label>
+                            <input type="text" class="form-control form-control-sm">
+                        </div>
+                    </div>
+
                 </div>
             </titled-card>
+            <titled-card class="mt-5" title="Past History">
+                <div class="row mt-4">
+                    <div class="col-sm-12 col-md-6 col-lg-3">
+                        <div class="form-check">
+                            <input class="form-check-input" id="p-unremarkable" type="checkbox" true-value="1"
+                                false-value="0">
+                            <label class="form-check-label" for="p-unremarkable"> Unremarkable </label>
+                        </div>
+                    </div>
+                    <div class="col-sm-12 col-md-6 col-lg-3">
+                        <div class="form-check">
+                            <input class="form-check-input" id="p-blood-d" type="checkbox" true-value="1" false-value="0">
+                            <label class="form-check-label" for="p-blood-d"> Blood Disease </label>
+                        </div>
+                    </div>
+                    <div class="col-sm-12 col-md-6 col-lg-3">
+                        <div class="form-check">
+                            <input class="form-check-input" id="p-asthma" type="checkbox" true-value="1" false-value="0">
+                            <label class="form-check-label" for="p-asthma"> Asthma </label>
+                        </div>
+                    </div>
+                    <div class="col-sm-12 col-md-6 col-lg-3">
+                        <div class="form-check">
+                            <input class="form-check-input" id="p-hyper" type="checkbox" true-value="1" false-value="0">
+                            <label class="form-check-label" for="p-hyper"> Hypertension </label>
+                        </div>
+                    </div>
+                </div>
+                <div class="row mt-5">
+                    <div class="col-sm-12 col-md-6 col-lg-3">
+                        <div class="form-check">
+                            <input class="form-check-input" id="p-cva" type="checkbox" true-value="1" false-value="0">
+                            <label class="form-check-label" for="p-cva"> CVA </label>
+                        </div>
+                    </div>
+                    <div class="col-sm-12 col-md-6 col-lg-3">
+                        <div class="form-check">
+                            <input class="form-check-input" id="p-gut" type="checkbox" true-value="1" false-value="0">
+                            <label class="form-check-label" for="p-gut"> GUT Disease</label>
+                        </div>
+                    </div>
+                    <div class="col-sm-12 col-md-6 col-lg-3">
+                        <div class="form-check">
+                            <input class="form-check-input" id="p-git" type="checkbox" true-value="1" false-value="0">
+                            <label class="form-check-label" for="p-git"> GIT Disease </label>
+                        </div>
+                    </div>
+                    <div class="col-sm-12 col-md-6 col-lg-3">
+                        <div class="form-check">
+                            <input class="form-check-input" id="p-pulmo" type="checkbox" true-value="1" false-value="0">
+                            <label class="form-check-label" for="p-pulmo"> Pulmo Disease </label>
+                        </div>
+                    </div>
+                </div>
+                <div class="row mt-5">
+                    <div class="col-sm-12 col-md-6 col-lg-3">
+                        <div class="form-check">
+                            <input class="form-check-input" id="p-heart" type="checkbox" true-value="1" false-value="0">
+                            <label class="form-check-label" for="p-heart"> Heart Disease </label>
+                        </div>
+                    </div>
+                    <div class="col-sm-12 col-md-6 col-lg-3">
+                        <div class="form-check">
+                            <input class="form-check-input" id="p-dm" type="checkbox" true-value="1" false-value="0">
+                            <label class="form-check-label" for="p-dm"> DM </label>
+                        </div>
+                    </div>
 
+                </div>
+                <div class="row mt-4">
+                    <div class="col-sm-12 col-md-6 col-lg-4">
+                        <div>
+                            <label class="form-label fs-6 mb-2 fw-semibold">Previous OR</label>
+                            <Textarea class="form-control form-control-sm" autoResize rows="5" cols="30" />
+                        </div>
+                    </div>
+                    <div class="col-sm-12 col-md-6 col-lg-4">
+                        <div>
+                            <label class="form-label fs-6 mb-2 fw-semibold">Previous Hospitalization</label>
+                            <Textarea class="form-control form-control-sm" autoResize rows="5" cols="30" />
+                        </div>
+                    </div>
+                    <div class="col-sm-12 col-md-6 col-lg-4">
+                        <div>
+                            <label class="form-label fs-6 mb-2 fw-semibold">Other Findings</label>
+                            <Textarea class="form-control form-control-sm" autoResize rows="5" cols="30" />
+                        </div>
+                    </div>
+                </div>
+            </titled-card>
+            <titled-card class="mt-5" title="Social History">
+                <div class="row mt-4">
+                    <div class="col-sm-12 col-md-6 col-lg-3">
+                        <div>
+                            <label class="form-label fs-6 mb-2 fw-semibold">Smoking</label>
+                            <Textarea class="form-control form-control-sm" autoResize rows="5" cols="30" />
+                        </div>
+                    </div>
+                    <div class="col-sm-12 col-md-6 col-lg-3">
+                        <div>
+                            <label class="form-label fs-6 mb-2 fw-semibold">Alcohol Intake</label>
+                            <Textarea class="form-control form-control-sm" autoResize rows="5" cols="30" />
+                        </div>
+                    </div>
+                    <div class="col-sm-12 col-md-6 col-lg-3">
+                        <div>
+                            <label class="form-label fs-6 mb-2 fw-semibold">Betel Nut Chewing</label>
+                            <Textarea class="form-control form-control-sm" autoResize rows="5" cols="30" />
+                        </div>
+                    </div>
+                    <div class="col-sm-12 col-md-6 col-lg-3">
+                        <div>
+                            <label class="form-label fs-6 mb-2 fw-semibold">Drug / Food Allergy</label>
+                            <Textarea class="form-control form-control-sm" autoResize rows="5" cols="30" />
+                        </div>
+                    </div>
+                </div>
+                <div class="row mt-3">
+                    <div class="col-sm-12 col-md-6 col-lg-6">
+                        <div>
+                            <label class="form-label fs-6 mb-2 fw-semibold">Others</label>
+                            <Textarea class="form-control form-control-sm" autoResize rows="5" cols="30" />
+                        </div>
+                    </div>
+
+                </div>
+            </titled-card>
         </div>
     </div>
 </template>
