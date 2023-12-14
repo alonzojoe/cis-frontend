5907c11079153e22ea41371b39599690d251fa44
Joenell Canlas Alonzo <medusazxc123@gmail.com>
2023-12-14 23:47:13 +0800

    Bug Fixes Loader

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,20 @@
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, ref, getCurrentInstance } from 'vue'
+import { useLoading } from '@/components/Loaders/loaderSettings';
 export default defineComponent({
   name: "App",
+  setup() {
+
+    const { loading } = useLoading()
+
+    return { loading }
+
+  }
 });
 </script>
 
 <template>
+  <Loading v-if="loading" />
   <router-view></router-view>
 </template>
 
