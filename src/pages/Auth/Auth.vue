d7b55f02ccda93893920ec03c833ac604e687bb0
Joenell Alonzo <72643848+alonzojoe@users.noreply.github.com>
2023-12-19 15:04:44 +0800

    Modify Auth User Interface

--- src/pages/Auth/Auth.vue
+++ src/pages/Auth/Auth.vue
@@ -1,50 +1,51 @@
 <template>
   <div class="authentication-wrapper authentication-cover authentication-bg">
     <div class="authentication-inner row">
-      <!-- /Left Text -->
-      <div class="d-none d-lg-flex col-lg-7 p-0">
-        <div class="auth-cover-bg auth-cover-bg-color d-flex justify-content-center align-items-center">
-          <img src="../../assets/img/illustrations/auth-login-illustration-light.png" alt="auth-login-cover"
-            class="img-fluid my-5 auth-illustration" data-app-light-img="illustrations/auth-login-illustration-light.png"
-            data-app-dark-img="illustrations/auth-login-illustration-dark.png" />
-
-          <img src="../../assets/img/illustrations/bg-shape-image-light.png" alt="auth-login-cover" class="platform-bg"
-            data-app-light-img="illustrations/bg-shape-image-light.png"
-            data-app-dark-img="illustrations/bg-shape-image-dark.png" />
+      <div
+        class="d-none d-lg-flex col-lg-8 p-0 bg-left align-items-center justify-content-center"
+        style="background: #7367f0"
+      >
+        <div
+          class="auth-cover-bg auth-cover-bg-color d-flex justify-content-center align-items-center"
+        >
+          <!-- <img
+            src="../../assets/images/bg.jpg"
+            alt="auth-login-cover"
+            class="img-fluid auth-illustration"
+            data-app-light-img="illustrations/auth-login-illustration-light.png"
+            data-app-dark-img="illustrations/auth-login-illustration-dark.png"
+          /> -->
         </div>
       </div>
-      <!-- /Left Text -->
 
-      <!-- Login -->
-      <div class="d-flex col-12 col-lg-5 align-items-center p-sm-5 p-4">
+      <div
+        class="d-flex col-12 col-lg-4 align-items-center p-sm-5 p-4 login-form"
+      >
         <div class="w-px-400 mx-auto">
-          <!-- Logo -->
           <div class="app-brand mb-4">
-            <a href="index.html" class="app-brand-link gap-2">
-              <span class="app-brand-logo demo">
-                <svg width="32" height="22" viewBox="0 0 32 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path fill-rule="evenodd" clip-rule="evenodd"
-                    d="M0.00172773 0V6.85398C0.00172773 6.85398 -0.133178 9.01207 1.98092 10.8388L13.6912 21.9964L19.7809 21.9181L18.8042 9.88248L16.4951 7.17289L9.23799 0H0.00172773Z"
-                    fill="#7367F0" />
-                  <path opacity="0.06" fill-rule="evenodd" clip-rule="evenodd"
-                    d="M7.69824 16.4364L12.5199 3.23696L16.5541 7.25596L7.69824 16.4364Z" fill="#161616" />
-                  <path opacity="0.06" fill-rule="evenodd" clip-rule="evenodd"
-                    d="M8.07751 15.9175L13.9419 4.63989L16.5849 7.28475L8.07751 15.9175Z" fill="#161616" />
-                  <path fill-rule="evenodd" clip-rule="evenodd"
-                    d="M7.77295 16.3566L23.6563 0H32V6.88383C32 6.88383 31.8262 9.17836 30.6591 10.4057L19.7824 22H13.6938L7.77295 16.3566Z"
-                    fill="#7367F0" />
-                </svg>
-              </span>
+            <a href="javascript:void(0);" class="app-brand-link gap-2">
+              <img
+                src="../../assets/logos/clinic-logo.png"
+                height="60px"
+                width="60px"
+                class="img-fluid"
+              />
             </a>
           </div>
-          <!-- /Logo -->
-          <h3 class="mb-1 fw-bold">Welcome to Vuexy! 👋</h3>
-          <p class="mb-4">Please sign-in to your account and start the adventure</p>
+
+          <h3 class="mb-1 fw-bold">Welcome to CIS! 👋</h3>
+          <p class="mb-4">Please sign-in to your account.</p>
 
           <form class="mb-3" @submit.prevent="login()">
             <div class="mb-3">
               <label for="email" class="form-label">Email</label>
-              <input type="text" class="form-control" id="email" name="email-username" v-model="formData.email" />
+              <input
+                type="text"
+                class="form-control"
+                id="email"
+                name="email-username"
+                v-model="formData.email"
+              />
             </div>
             <div class="mb-3 form-password-toggle">
               <div class="d-flex justify-content-between">
@@ -54,61 +55,39 @@
                 </a>
               </div>
               <div class="input-group input-group-merge">
-                <input type="password" id="password" class="form-control" name="password" aria-describedby="password"
-                  v-model="formData.password" />
-                <span class="input-group-text cursor-pointer"><i class="ti ti-eye-off"></i></span>
+                <input
+                  type="password"
+                  id="password"
+                  class="form-control"
+                  name="password"
+                  aria-describedby="password"
+                  v-model="formData.password"
+                />
+                <span class="input-group-text cursor-pointer"
+                  ><i class="ti ti-eye-off"></i
+                ></span>
               </div>
             </div>
-            <div class="mb-3">
-              <div class="form-check">
-                <input class="form-check-input" type="checkbox" id="remember-me" />
-                <label class="form-check-label" for="remember-me"> Remember Me </label>
-              </div>
-            </div>
-            <button type="submit" class="btn btn-primary d-grid w-100">Sign in</button>
+
+            <button type="submit" class="mt-3 btn btn-primary d-grid w-100">
+              Sign in
+            </button>
           </form>
-
-          <p class="text-center">
-            <span>New on our platform?</span>
-            <a href="auth-register-cover.html">
-              <span>Create an account</span>
-            </a>
-          </p>
-
-          <div class="divider my-4">
-            <div class="divider-text">or</div>
-          </div>
-
-          <div class="d-flex justify-content-center">
-            <a href="javascript:;" class="btn btn-icon btn-label-facebook me-3">
-              <i class="tf-icons fa-brands fa-facebook-f fs-5"></i>
-            </a>
-
-            <a href="javascript:;" class="btn btn-icon btn-label-google-plus me-3">
-              <i class="tf-icons fa-brands fa-google fs-5"></i>
-            </a>
-
-            <a href="javascript:;" class="btn btn-icon btn-label-twitter">
-              <i class="tf-icons fa-brands fa-twitter fs-5"></i>
-            </a>
-          </div>
         </div>
       </div>
-      <!-- /Login -->
     </div>
   </div>
-  {{ formData }}
 </template>
 
 <script lang="ts">
 import { defineComponent, ref, computed, inject, onMounted, watch } from "vue";
-import '@/assets/vendor/css/pages/page-auth.css'
+import "@/assets/vendor/css/pages/page-auth.css";
 import api from "@/api";
 import Cookies from "js-cookie";
 import { useStore } from "vuex";
 import { useRouter } from "vue-router";
 import CryptoJS from "crypto-js";
-import { swalMessage } from '@/service'
+import { swalMessage } from "@/service";
 
 export default defineComponent({
   name: "Auth",
@@ -119,7 +98,6 @@ export default defineComponent({
 
     const formData = computed(() => store.getters.getLoginForm);
     const user = computed(() => store.getters.getAuthenticatedUser);
-
 
     const isLoading = ref(false);
     const login = async () => {
@@ -170,15 +148,29 @@ export default defineComponent({
           isLoading.value = false;
         });
       }
-    }
+    };
 
     onMounted(() => {
-      store.commit('resetLoginForm');
-    })
+      store.commit("resetLoginForm");
+    });
 
     return { formData, login };
   },
 });
 </script>
 
-<style scoped></style>
+<style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap");
+.bg-left {
+  background-color: #7367f0 !important;
+}
+.auth-cover-bg {
+  margin: 2rem !important;
+  background: url("../../assets/images/bg-2.jpg");
+  background-size: cover;
+  background-position: center;
+  box-shadow: 10px 10px 31px -1px rgba(0, 0, 0, 0.52);
+  -webkit-box-shadow: 10px 10px 31px -1px rgba(0, 0, 0, 0.52);
+  -moz-box-shadow: 10px 10px 31px -1px rgba(0, 0, 0, 0.52);
+}
+</style>
