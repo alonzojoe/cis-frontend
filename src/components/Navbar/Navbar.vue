6e9ebb7103a533cddc7731b8b9c09e972944ecf7
Joenell Alonzo <72643848+alonzojoe@users.noreply.github.com>
2024-01-02 10:21:00 +0800

    Navbar UI UX Adjustments

--- src/components/Navbar/Navbar.vue
+++ src/components/Navbar/Navbar.vue
@@ -1,64 +1,93 @@
 <template>
   <nav
     class="m-0 rounded-0 w-auto layout-navbar navbar navbar-expand-xl navbar-detached align-items-center bg-navbar-theme container-fluid"
-    id="layout-navbar">
-    <div class="layout-menu-toggle navbar-nav align-items-xl-center me-3 me-xl-0 d-xl-none">
-      <a class="nav-item nav-link px-0 me-xl-4" @click="toggleExpend" href="javascript:void(0)">
+    id="layout-navbar"
+  >
+    <div
+      class="layout-menu-toggle navbar-nav align-items-xl-center me-3 me-xl-0 d-xl-none"
+    >
+      <a
+        class="nav-item nav-link px-0 me-xl-4"
+        @click="toggleExpend"
+        href="javascript:void(0)"
+      >
         <i class="ti ti-menu-2 ti-sm"></i>
       </a>
     </div>
 
-    <div class="navbar-nav-right d-flex align-items-center" id="navbar-collapse">
-
+    <div
+      class="navbar-nav-right d-flex align-items-center"
+      id="navbar-collapse"
+    >
       <div class="navbar-nav align-items-center">
-        <div class="nav-item navbar-search-wrapper mb-0">
+        <div class="nav-item navbar-search-wrapper mb-2">
           <h4 class="mt-4 fw-bold">{{ route.meta.title }}</h4>
         </div>
       </div>
 
-
       <ul class="navbar-nav flex-row align-items-center ms-auto">
-
-
         <li class="nav-item dropdown-language dropdown me-2 me-xl-0">
-          <a class="nav-link hide-arrow" href="javascript:void(0);" @click="
-            toggleChangeTheme(
-              appTheme == 'dark-theme' ? 'white-theme' : 'dark-theme'
-            )
-            ">
-            <i class="ti rounded-circle ti-md" :class="appTheme == 'dark-theme' ? 'ti-sun-filled' : 'ti-moon-filled'
-              ">
-
+          <a
+            class="nav-link hide-arrow"
+            href="javascript:void(0);"
+            @click="
+              toggleChangeTheme(
+                appTheme == 'dark-theme' ? 'white-theme' : 'dark-theme'
+              )
+            "
+          >
+            <i
+              class="ti rounded-circle ti-md"
+              :class="
+                appTheme == 'dark-theme' ? 'ti-sun-filled' : 'ti-moon-filled'
+              "
+            >
             </i>
           </a>
         </li>
 
-
-        <li class="nav-item dropdown-language dropdown me-2 me-xl-0" id="user-name">
+        <li
+          class="nav-item dropdown-language dropdown me-2 me-xl-0"
+          id="user-name"
+        >
           <a class="nav-link hide-arrow" href="javascript:void(0);">
-            <h5 class="mt-3 fw-semibold">{{ authUser.fname }} {{ authUser.lname }}</h5>
+            <h5 class="mt-3 fw-semibold">
+              {{ authUser.fname }} {{ authUser.lname }}
+            </h5>
           </a>
         </li>
 
-
-
         <li class="nav-item navbar-dropdown dropdown-user dropdown">
-          <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);" data-bs-toggle="dropdown">
+          <a
+            class="nav-link dropdown-toggle hide-arrow"
+            href="javascript:void(0);"
+            data-bs-toggle="dropdown"
+          >
             <div class="avatar avatar-online">
-              <img src="../../assets/logos/avatar.jpeg" alt="user-avatar" class="h-auto rounded-circle" />
+              <img
+                src="../../assets/logos/avatar.jpeg"
+                alt="user-avatar"
+                class="h-auto rounded-circle"
+              />
             </div>
           </a>
         </li>
-
       </ul>
     </div>
   </nav>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref, watch, watchEffect, computed, onMounted } from "vue";
+import {
+  defineComponent,
+  ref,
+  watch,
+  watchEffect,
+  computed,
+  onMounted,
+} from "vue";
 import { useRoute } from "vue-router";
-import { useStore } from "vuex"
+import { useStore } from "vuex";
 export default defineComponent({
   props: {
     expanded: Boolean,
@@ -67,7 +96,7 @@ export default defineComponent({
     const route = useRoute();
     const isExpanded = ref(false);
     const store = useStore();
-    const authUser = computed(() => store.getters.getAuthenticatedUser)
+    const authUser = computed(() => store.getters.getAuthenticatedUser);
     const toggleExpend = () => {
       isExpanded.value = !isExpanded.value ? true : false;
     };
@@ -90,7 +119,6 @@ export default defineComponent({
     const appTheme = ref("");
     const body = document.body;
     const toggleChangeTheme = async (theme) => {
-
       appTheme.value = theme;
       localStorage.setItem("app-theme", theme);
       body.classList.add("fade-effect", "fade-out");
@@ -133,13 +161,11 @@ export default defineComponent({
       }
     };
 
-
     const removeDarkTheme = () => {
       const linkTag = document.getElementById("dark-mode");
       if (linkTag) {
         document.head.removeChild(linkTag);
       }
-
     };
 
     const title = ref("");
@@ -156,7 +182,7 @@ export default defineComponent({
       theme == null || theme == "white-theme"
         ? toggleChangeTheme("white-theme")
         : toggleChangeTheme("dark-theme");
-    })
+    });
 
     return {
       isExpanded,
@@ -166,7 +192,7 @@ export default defineComponent({
       appTheme,
       title,
       route,
-      authUser
+      authUser,
     };
   },
 });
